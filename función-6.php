a508208c97c1de00de18b1f00097b5eb062a7d39
Diego2525 <diego.portero@epn.edu.ec>
2019-01-17 01:34:44 -0500

    funcion 6

---
+++ función-6.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html class="nojs html css_verticalspacer" lang="en-US">
+ <head>
+
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <meta name="generator" content="2018.1.0.386"/>
+  
+  <script type="text/javascript">
+   // Update the 'nojs'/'js' class on the html node
+document.documentElement.className = document.documentElement.className.replace(/\bnojs\b/g, 'js');
+
+// Check that all required assets are uploaded and up-to-date
+if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["museutils.js", "museconfig.js", "jquery.musemenu.js", "jquery.watch.js", "jquery.musepolyfill.bgsize.js", "require.js", "funci_n-4.css"], "outOfDate":[]};
+</script>
+  
+  <link rel="shortcut icon" href="images/favicon.ico?crc=410785846"/>
+  <title>Función 4</title>
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="css/site_global.css?crc=83274698"/>
+  <link rel="stylesheet" type="text/css" href="css/funci_n-4.css?crc=64665871" id="pagesheet"/>
+  <link rel="stylesheet" type="text/css" href="css/index.css?crc=3946665806" id="pagesheet"/>
+  <link rel="stylesheet" type="text/css" href="css/semantic.css"/>
+  <!-- JS includes -->
+  <!--[if lt IE 9]>
+  <script src="scripts/html5shiv.js?crc=4241844378" type="text/javascript"></script>
+  <![endif]-->
+   </head>
+ <body>
+
+  <div class="clearfix" id="page"><!-- group -->
+   <div class="browser_width grpelem" id="u2903-bw">
+    <div id="u2903"><!-- group -->
+     <div class="clearfix" id="u2903_align_to_page">
+      <div class="clearfix grpelem" id="pmenuu2916"><!-- column -->
+       <nav class="MenuBar clearfix colelem" id="menuu2916"><!-- vertical box -->
+        <div class="MenuItemContainer clearfix colelem" id="u2945"><!-- horizontal box -->
+         <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition rounded-corners clearfix grpelem" id="u2948" href="index.html"><!-- horizontal box --><div class="MenuItemLabel clearfix grpelem" id="u2951-4"><!-- content --><p id="u2951-2">Bienvenido</p></div></a>
+        </div>
+        <div class="MenuItemContainer clearfix colelem" id="u2938"><!-- horizontal box -->
+         <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition rounded-corners clearfix grpelem" id="u2939" href="funci%c3%b3n-1.php"><!-- horizontal box --><div class="MenuItemLabel clearfix grpelem" id="u2940-4"><!-- content --><p id="u2940-2">Función 1</p></div></a>
+        </div>
+        <div class="MenuItemContainer clearfix colelem" id="u2924"><!-- horizontal box -->
+         <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition rounded-corners clearfix grpelem" id="u2927" href="funci%c3%b3n-2.php"><!-- horizontal box --><div class="MenuItemLabel clearfix grpelem" id="u2929-4"><!-- content --><p id="u2929-2">Función 2</p></div></a>
+        </div>
+        <div class="MenuItemContainer clearfix colelem" id="u2931"><!-- horizontal box -->
+         <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition rounded-corners clearfix grpelem" id="u2932" href="funci%c3%b3n-3.php"><!-- horizontal box --><div class="MenuItemLabel clearfix grpelem" id="u2935-4"><!-- content --><p id="u2935-2">Función 3</p></div></a>
+        </div>
+        <div class="MenuItemContainer clearfix colelem" id="u2917"><!-- horizontal box -->
+         <a class="nonblock nontext MenuItem MenuItemWithSubMenu MuseMenuActive transition rounded-corners clearfix grpelem" id="u2920" href="funci%c3%b3n-4.php"><!-- horizontal box --><div class="MenuItemLabel clearfix grpelem" id="u2923-4"><!-- content --><p id="u2923-2">Función 4</p></div></a>
+        </div>
+        <div class="MenuItemContainer clearfix colelem" id="u2931"><!-- horizontal box -->
+          <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition rounded-corners clearfix grpelem" id="u2932" href="ProyectoFinal/Proyecto.php"><!-- horizontal box --><div class="MenuItemLabel clearfix grpelem" id="u106-4"><!-- content --><p id="u106-2">Función 5</p></div></a>
+         </div>
+         <div class="MenuItemContainer clearfix colelem" id="u2931"><!-- horizontal box -->
+          <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition rounded-corners clearfix grpelem" id="u2932" href="funci%c3%b3n-6.php"><!-- horizontal box --><div class="MenuItemLabel clearfix grpelem" id="u106-4"><!-- content --><p id="u106-2">Función 6</p></div></a>
+         </div>
+       </nav>
+       <a class="nonblock nontext clearfix colelem" id="u2905-5" href="http://www.musefree.com"><!-- content --><p>Made with <span class="actAsInlineDiv normal_text" id="u2906"><!-- content --><span class="actAsDiv clip_frame excludeFromNormalFlow" id="u2907"><!-- image --><img id="u2907_img" src="images/heart.png?crc=117585526" alt="" width="11" height="9"/></span></span> by °Kelvin</p></a>
+      </div>
+      <div class="clearfix grpelem" id="pu2909-4"><!-- column -->
+       <div class="h1 clearfix colelem" id="u2909-4"><!-- content -->
+        <h1>Función 4 - Integral por metodo de Simpson</h1>
+       </div>
+       <div class="colelem" id="u2958"><!-- simple frame --></div>
+       <div class="text clearfix colelem shared_content" id="u139-4" data-content-guid="u139-4_content" style="color: black;font-size: 15px;"><!-- content -->                  
+        
+        <form action="función-3.php" method="post">
+           <div class="ui input">
+             <input type="text" placeholder="Función: x**2" name="y">
+           </div><br><br>
+           <div class="ui input">
+             <input type="text" placeholder="Inicio" name="a">
+           </div>
+           <div class="ui input">
+             <input type="text" placeholder="Fin" name="b">
+           </div><br><br>
+           <div class="ui input">
+             <input type="text" placeholder="Cantidad de rectangulos" name="n">
+           </div><br><br>
+           <button class="ui button" type="submit" value="Cargar">
+             Calcular
+           </button><br><br>
+         </form>
+         <?php
+           if(isset($_POST['y'])&&isset($_POST['a'])&&isset($_POST['b'])&&isset($_POST['n'])){
+             $y=$_POST['y'];
+             $a=$_POST['a'];
+             $b=$_POST['b'];
+             $n=$_POST['n'];
+             include 'funciones/IntegralRectangulo.php';  
+             echo rectangulo($y,$a,$b,$n);
+           }
+         ?>
+         </div>
+      </div>
+     </div>
+    </div>
+   </div>
+   <div class="browser_width grpelem" id="u2954-bw">
+    <div class="gradient" id="u2954"><!-- group -->
+     <div class="clearfix" id="u2954_align_to_page">
+      <div class="clearfix grpelem" id="u2959"><!-- group -->
+       <a class="nonblock nontext museBGSize rounded-corners grpelem" id="u2960" href="index.html"><!-- simple frame --></a>
+      </div>
+      <a class="nonblock nontext rounded-corners clearfix grpelem" id="u2955-4" href="https://www.epn.edu.ec/"><!-- content --><p>Sigue a mis creadores</p></a>
+      <a class="nonblock nontext museBGSize grpelem" id="u2911" href="https://www.instagram.com/memesepn/?hl=es"><!-- content --></a>
+      <a class="nonblock nontext museBGSize grpelem" id="u2902" href="https://twitter.com/EPNEcuador"><!-- content --></a>
+      <a class="nonblock nontext museBGSize grpelem" id="u2915" href="https://www.facebook.com/EPNQuito"><!-- content --></a>
+     </div>
+    </div>
+   </div>
+   <div class="museBGSize grpelem" id="u2957"><!-- content --></div>
+   <div class="verticalspacer" data-offset-top="930" data-content-above-spacer="930" data-content-below-spacer="0"></div>
+  </div>
+  <!-- Other scripts -->
+  <script type="text/javascript">
+   // Decide whether to suppress missing file error or not based on preference setting
+var suppressMissingFileError = false
+</script>
+  <script type="text/javascript">
+   window.Muse.assets.check=function(c){if(!window.Muse.assets.checked){window.Muse.assets.checked=!0;var b={},d=function(a,b){if(window.getComputedStyle){var c=window.getComputedStyle(a,null);return c&&c.getPropertyValue(b)||c&&c[b]||""}if(document.documentElement.currentStyle)return(c=a.currentStyle)&&c[b]||a.style&&a.style[b]||"";return""},a=function(a){if(a.match(/^rgb/))return a=a.replace(/\s+/g,"").match(/([\d\,]+)/gi)[0].split(","),(parseInt(a[0])<<16)+(parseInt(a[1])<<8)+parseInt(a[2]);if(a.match(/^\#/))return parseInt(a.substr(1),
+16);return 0},f=function(f){for(var g=document.getElementsByTagName("link"),j=0;j<g.length;j++)if("text/css"==g[j].type){var l=(g[j].href||"").match(/\/?css\/([\w\-]+\.css)\?crc=(\d+)/);if(!l||!l[1]||!l[2])break;b[l[1]]=l[2]}g=document.createElement("div");g.className="version";g.style.cssText="display:none; width:1px; height:1px;";document.getElementsByTagName("body")[0].appendChild(g);for(j=0;j<Muse.assets.required.length;){var l=Muse.assets.required[j],k=l.match(/([\w\-\.]+)\.(\w+)$/),i=k&&k[1]?
+k[1]:null,k=k&&k[2]?k[2]:null;switch(k.toLowerCase()){case "css":i=i.replace(/\W/gi,"_").replace(/^([^a-z])/gi,"_$1");g.className+=" "+i;i=a(d(g,"color"));k=a(d(g,"backgroundColor"));i!=0||k!=0?(Muse.assets.required.splice(j,1),"undefined"!=typeof b[l]&&(i!=b[l]>>>24||k!=(b[l]&16777215))&&Muse.assets.outOfDate.push(l)):j++;g.className="version";break;case "js":j++;break;default:throw Error("Unsupported file type: "+k);}}c?c().jquery!="1.8.3"&&Muse.assets.outOfDate.push("jquery-1.8.3.min.js"):Muse.assets.required.push("jquery-1.8.3.min.js");
+g.parentNode.removeChild(g);if(Muse.assets.outOfDate.length||Muse.assets.required.length)g="Puede que determinados archivos falten en el servidor o sean incorrectos. Limpie la cache del navegador e inténtelo de nuevo. Si el problema persiste, póngase en contacto con el administrador del sitio web.",f&&Muse.assets.outOfDate.length&&(g+="\nOut of date: "+Muse.assets.outOfDate.join(",")),f&&Muse.assets.required.length&&(g+="\nMissing: "+Muse.assets.required.join(",")),suppressMissingFileError?(g+="\nUse SuppressMissingFileError key in AppPrefs.xml to show missing file error pop up.",console.log(g)):alert(g)};location&&location.search&&location.search.match&&location.search.match(/muse_debug/gi)?
+setTimeout(function(){f(!0)},5E3):f()}};
+var muse_init=function(){require.config({baseUrl:""});require(["jquery","museutils","whatinput","jquery.musemenu","jquery.watch","jquery.musepolyfill.bgsize"],function(c){var $ = c;$(document).ready(function(){try{
+window.Muse.assets.check($);/* body */
+Muse.Utils.transformMarkupToFixBrowserProblemsPreInit();/* body */
+Muse.Utils.prepHyperlinks(true);/* body */
+Muse.Utils.resizeHeight('.browser_width');/* resize height */
+Muse.Utils.requestAnimationFrame(function() { $('body').addClass('initialized'); });/* mark body as initialized */
+Muse.Utils.makeButtonsVisibleAfterSettingMinWidth();/* body */
+Muse.Utils.initWidget('.MenuBar', ['#bp_infinity'], function(elem) { return $(elem).museMenu(); });/* unifiedNavBar */
+Muse.Utils.fullPage('#page');/* 100% height page */
+Muse.Utils.showWidgetsWhenReady();/* body */
+Muse.Utils.transformMarkupToFixBrowserProblems();/* body */
+}catch(b){if(b&&"function"==typeof b.notify?b.notify():Muse.Assert.fail("Error calling selector function: "+b),false)throw b;}})})};
+
+</script>
+  <!-- RequireJS script -->
+  <script src="scripts/require.js?crc=7928878" type="text/javascript" async data-main="scripts/museconfig.js?crc=4286661555" onload="if (requirejs) requirejs.onError = function(requireType, requireModule) { if (requireType && requireType.toString && requireType.toString().indexOf && 0 <= requireType.toString().indexOf('#scripterror')) window.Muse.assets.check(); }" onerror="window.Muse.assets.check();"></script>
+   </body>
+</html>
